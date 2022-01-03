1708d840f5a97cdb480f96723b0b3bb3d3aadac6
Jackson Calixto <HZJ0RQ@LAAM.corp.gm.com>
2022-01-03 10:13:15 -0300

    Modifying Data

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -3,6 +3,7 @@ using CoreCodeCamp.Data;
 using CoreCodeCamp.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,15 +11,18 @@ using System.Threading.Tasks;
 namespace CoreCodeCamp.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class CampsController : ControllerBase
     {
         private readonly ICampRepository _repository;
         private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
 
-        public CampsController(ICampRepository repository, IMapper mapper)
+        public CampsController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
         {
             _repository = repository;
             _mapper = mapper;
+            _linkGenerator = linkGenerator;
         }
 
         // GET http://localhost:6600/api/camps
@@ -72,8 +76,45 @@ namespace CoreCodeCamp.Controllers
             }
             catch (System.Exception)
             {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");                ;
+            }
+        }
+
+        /* POST http://localhost:6600/api/camps 
+         {
+	        "name": "San Diego Code Camp",
+	        "moniker" : "SD2018",
+	        "eventDate" : "2018-05-05",
+	        "venue" : "SD Comunity College",
+	        "locationPostalCode" : "12345"
+         }
+        */
+        [HttpPost]
+        public async Task<ActionResult<CampModel>> Post(CampModel model)
+        {
+            try
+            {
+                var location = _linkGenerator.GetPathByAction("Get", "Camps", new { moniker = model.Moniker });
+
+                if (string.IsNullOrWhiteSpace(location))
+                {
+                    return BadRequest("Could not use current moniker");
+                }
+                // Create a new Camp
+
+                var camp = _mapper.Map<Camp>(model);
+                _repository.Add(camp);  
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Created($"/api/camp/{camp.Moniker}", _mapper.Map<CampModel>(camp));
+                }
+
+                return Ok();
+            }
+            catch (Exception)
+            {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
-                throw;
             }
         }
     }
